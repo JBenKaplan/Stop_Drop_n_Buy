1b37bb4d3e22e8ed04e2aeefe74cc2ed3231222e
Ben Kaplan <bjminkaplan@gmail.com>
2022-12-11 20:11:02 -0600

    Updated navbar for cart display

---
+++ client/src/components/CartCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="cart">
+    <p>test</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CartCard'
+}
+</script>
+
+<style scoped>
+
+</style>
--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -3,16 +3,24 @@
     <router-link to="/" class="navitem"><img src="../assets/home-page--v1.png" alt="home"
         class="navitem" /></router-link>
     <router-link to="/products" class="navitem">Products</router-link>
-    <div>
-      <img src="../assets/shopping-cart--v1.png" alt="cart" class="navitem" />
+    <div class="navitem">
+      <img src="../assets/shopping-cart--v1.png" alt="cart" class="navitem" @click="handleClick" />
+      <div className="dropdown-panel {{panelDisplay}}">
+        <p>Test Hi</p>
+      </div>
       <!-- <img src="../assets/search-more.png" alt="search" class="navitem" /> -->
     </div>
   </nav>
 </template>
 
 <script>
+let cart = false
+import { useVuex } from '@vueblock/vue-use-vuex'
+
 export default {
-  name: 'NavBar'
+  name: 'NavBar',
+  methods: {
+  }
 }
 </script>
 
