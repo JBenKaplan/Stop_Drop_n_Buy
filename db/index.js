aa39c44892919e4f6fb32cc0e57a83b590ea29b6
Ben Kaplan <bjminkaplan@gmail.com>
2022-12-14 17:40:30 -0600

    URI update

--- db/index.js
+++ db/index.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 require('dotenv').config() // Add this line
 
-// let MONGODB_URI = 'mongodb://127.0.0.1:27017/stopDropNBuy'
-let MONGODB_URI =
-  process.env.NODE_ENV === 'production'
-    ? process.env.MONGODB_URI
-    : 'mongodb://127.0.0.1:27017/StopDropBuy'
+let MONGODB_URI = 'mongodb://127.0.0.1:27017/stopDropNBuy'
+// let MONGODB_URI =
+//   process.env.NODE_ENV === 'production'
+//     ? process.env.MONGODB_URI
+//     : 'mongodb://127.0.0.1:27017/StopDropBuy'
 
 mongoose
   .connect(MONGODB_URI)
